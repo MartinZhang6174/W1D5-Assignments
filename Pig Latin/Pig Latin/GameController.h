1d8fc8936366b74c9e345e6cac4ed8649b499c64
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-07-01 18:35:24 -0700

    screw this

---
+++ Pig Latin/Pig Latin/GameController.h
@@ -0,0 +1,13 @@
+//
+//  GameController.h
+//  Pig Latin
+//
+//  Created by Martin Zhang on 2016-07-01.
+//  Copyright © 2016 Martin Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameController : NSObject
+
+@end
---
+++ Pig Latin/Pig Latin/GameController.m
@@ -0,0 +1,13 @@
+//
+//  GameController.m
+//  Pig Latin
+//
+//  Created by Martin Zhang on 2016-07-01.
+//  Copyright © 2016 Martin Zhang. All rights reserved.
+//
+
+#import "GameController.h"
+
+@implementation GameController
+
+@end
