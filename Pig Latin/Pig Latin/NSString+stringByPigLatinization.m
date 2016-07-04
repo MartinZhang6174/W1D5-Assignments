052ae67d302a47cee4d4aa88101f662d76890769
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-07-03 17:16:24 -0700

    Screw you pig latin

--- Pig Latin/Pig Latin/NSString+stringByPigLatinization.m
+++ Pig Latin/Pig Latin/NSString+stringByPigLatinization.m
@@ -14,12 +14,17 @@
     
     NSCharacterSet *vowels = [NSCharacterSet characterSetWithCharactersInString:@"aeiou"];
     NSRange vowelRange = [self rangeOfCharacterFromSet:(NSCharacterSet*) vowels];
-    NSString *leftPart = [self substringToIndex:vowelRange.location];
-    NSString *rightPart = [self substringFromIndex:vowelRange.location];
-    NSString *ending = @"ay";
-    NSString *resultString = [NSString stringWithFormat:@"%@%@%@",rightPart,leftPart,ending];
-    return resultString;
-    
+    if (vowelRange.location == NSNotFound) {
+        NSLog(@"The first character is not a consonant but a vowel.");
+    } else {
+        NSString *leftPart = [self substringToIndex:vowelRange.location];
+        NSString *rightPart = [self substringFromIndex:vowelRange.location];
+        NSString *ending = @"ay";
+        NSString *resultString = [NSString stringWithFormat:@"%@%@%@",rightPart,leftPart,ending];
+        return resultString;
+    }
+
+    return self;
 // 1) character set with vowels 
 // 2) [self rangeOfCharacter...] -> range of first vowel (or range.location == NSNotFound)
 // 3) [self substringToIndex:range.location] --> left part
